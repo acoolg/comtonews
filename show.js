ec79b1d4b13e5d055d703a4a24d19e57fdcae5f0
Your Name <you@example.com>
2023-10-09 21:35:45 +0800

    set every thing to json

--- show.js
+++ show.js
@@ -30,3 +30,27 @@ fetch('news.json')
     .catch(error => {
         console.error('发生错误：', error);
     });
+
+    // 获取 JSON 数据并处理
+fetch('list.json')
+.then(response => response.json())
+.then(jsonData => {
+  // 初始化一个空字符串来存储生成的 HTML
+  let htmlString = '';
+
+  // 遍历 JSON 数据数组
+  jsonData.forEach(item => {
+    // 为每个 JSON 对象生成相应的 HTML 字符串，并将其添加到 htmlString 中
+    htmlString += `
+      <li>
+        <a href="${item.link}" class="list-link" title="${item.title}">${item.text}</a>
+      </li>
+    `;
+  });
+
+  // 然后，您可以将 htmlString 插入到您的文档中，例如：
+  document.getElementById('list').innerHTML = htmlString;
+})
+.catch(error => {
+  console.error('发生错误：', error);
+});
